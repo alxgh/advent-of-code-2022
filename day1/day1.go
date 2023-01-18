a06fa168ab93c3339f7b020cf62b91a375bf67de
Alex Gholamian <mmghk20gmail.com>
2023-01-18 14:01:27 +0330

    day1-part1-golang

---
+++ day1/day1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("inp.txt")
+	defer func() { _ = f.Close() }()
+
+	if err != nil {
+		panic(err)
+	}
+	sc := bufio.NewScanner(f)
+	sc.Split(bufio.ScanLines)
+	max := 0
+	current := 0
+	for sc.Scan() {
+		n := sc.Text()
+		if len(n) == 0 {
+			if current > max {
+				max = current
+			}
+			current = 0
+			continue
+		}
+		i, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		current += i
+	}
+	if current > max {
+		max = current
+	}
+	fmt.Println("MAX:", max)
+}
